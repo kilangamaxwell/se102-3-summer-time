c2ef9f33cd756fddb196bd668f4d82ba3d329e70
Robert Maxwell <robinix585@gmail.com>
2023-04-10 11:08:22 -0600

    Initial upload

---
+++ summer.py
@@ -0,0 +1,13 @@
+
+def main():
+    # Remove the "pass"
+    # Write your code here
+    sum = 0
+    while sum < 1000:
+      number = int(input("Enter your number: "))
+      sum += number
+    
+    print(f"Final sum: {sum}")
+
+if __name__ == "__main__":
+    main()
